120309c5008714175c3857caeb2a83ad1f5f913d
Daiant <cmg2512@gmail.com>
2022-05-23 09:47:24 +0200

    ssl

--- server/db.js
+++ server/db.js
@@ -11,7 +11,7 @@ const credentials = {
 }
 const connectionString = 'postgresql://bwspjcvcbiswyz:1e29548d9fb5baeaa641b8d4f4e6ceb7f8540e8893e732c10300e0921925969e@ec2-52-30-159-47.eu-west-1.compute.amazonaws.com:5432/ddrfc8p3krbeb9';
 
-const pool = new pg.Pool({connectionString});
+const pool = new pg.Pool({connectionString, ssl: {rejectUnauthorized: false}});
 
 module.exports = {
     pool
